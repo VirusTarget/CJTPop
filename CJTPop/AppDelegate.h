a7ffbd494f06f7ca2bde05fa41cb971ec3641e41
VirusTarget <chenjintian@xxcc.local>
2016-10-17 12:03:46 +0800

    Initial Commit

---
+++ CJTPop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJTPop
+//
+//  Created by chenjintian on 16/10/17.
+//  Copyright © 2016年 CJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJTPop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJTPop
+//
+//  Created by chenjintian on 16/10/17.
+//  Copyright © 2016年 CJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
